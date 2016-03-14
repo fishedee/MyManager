8f4cbb614037594d83b9e5a916d7bd3c873464e0
fishedee <306766045@qq.com>
2016-03-14 15:26:47 +0800

    优化双引号的html展示

--- server/models/blog/bloggitao.go
+++ server/models/blog/bloggitao.go
@@ -31,7 +31,27 @@ func (this *BlogGitAoModel) getFileContent(fileAddress string) string {
 }
 
 func (this *BlogGitAoModel) markdownToHtml(data string) string {
-	return string(blackfriday.MarkdownCommon([]byte(data)))
+	htmlOption := 0 |
+		blackfriday.HTML_USE_XHTML |
+		blackfriday.HTML_USE_SMARTYPANTS |
+		blackfriday.HTML_SMARTYPANTS_FRACTIONS |
+		blackfriday.HTML_SMARTYPANTS_DASHES |
+		blackfriday.HTML_SMARTYPANTS_LATEX_DASHES |
+		blackfriday.HTML_SMARTYPANTS_ANGLED_QUOTES
+	extensionOption := 0 |
+		blackfriday.EXTENSION_NO_INTRA_EMPHASIS |
+		blackfriday.EXTENSION_TABLES |
+		blackfriday.EXTENSION_FENCED_CODE |
+		blackfriday.EXTENSION_AUTOLINK |
+		blackfriday.EXTENSION_STRIKETHROUGH |
+		blackfriday.EXTENSION_SPACE_HEADERS |
+		blackfriday.EXTENSION_HEADER_IDS |
+		blackfriday.EXTENSION_BACKSLASH_LINE_BREAK |
+		blackfriday.EXTENSION_DEFINITION_LISTS
+	renderer := blackfriday.HtmlRenderer(htmlOption, "", "")
+	result := blackfriday.MarkdownOptions([]byte(data), renderer, blackfriday.Options{
+		Extensions: extensionOption})
+	return string(result)
 }
 
 func (this *BlogGitAoModel) convertImage(dir string, content string) string {
