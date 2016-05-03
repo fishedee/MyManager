c75c872af2d554a24a210044a1c2484ddd7e2bcb
fishedee <306766045@qq.com>
2016-05-03 17:13:01 +0800

    调整test的接口

--- server/models/common/basetest.go
+++ server/models/common/basetest.go
@@ -2,13 +2,12 @@ package common
 
 import (
 	. "github.com/fishedee/web"
-	"testing"
 )
 
 type BaseTest struct {
 	BeegoValidateTest
 }
 
-func InitTest(t *testing.T, test BeegoValidateTestInterface) {
-	InitBeegoVaildateTest(t, test)
+func InitTest(test BeegoValidateTestInterface) {
+	InitBeegoVaildateTest(test)
 }
