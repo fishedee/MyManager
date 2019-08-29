3b097ed07f6d0fe1d208533781e1e16bf2a9ac6c
fishedee <306766045@qq.com>
2019-08-29 13:31:18 +0800

    加入mysql_async

--- server2/src/main.rs
+++ server2/src/main.rs
@@ -1,19 +1,40 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, Error,HttpRequest, HttpServer, Responder,HttpResponse,error};
 use actix_web::middleware::Logger;
 use listenfd::ListenFd;
 use env_logger;
+use futures::future::{ok, Future};
+
+extern crate mysql_async as mysql;
+use mysql::prelude::*;
+use mysql::Pool;
+
+fn index1(pool:web::Data<Pool>)->impl Future<Item=String,Error=Error>{
+	let conn = pool.get_conn();
+
+	conn.and_then(|conn|{
+		conn.query("select categoryId,userId,name,remark from t_category")
+	}).and_then(|data|{
+		data.collect::<(u32,String,String,String)>()
+	}).map(|(_,data)|{
+		format!("{:?}",data)
+	}).map_err(|e|{
+		error::ErrorInternalServerError(e)
+	})
+}
 
 fn main() {
+	let database_url = "mysql://root:1@localhost:3306/FishMoney";
+	let pool = mysql::Pool::new(database_url);
+
 	std::env::set_var("RUST_LOG", "actix_web=info");
 	env_logger::init();
 
 	let mut listenfd = ListenFd::from_env();
-    let mut server = HttpServer::new(|| {
+    let mut server = HttpServer::new(move|| {
         App::new()
         	.wrap(Logger::default())
-            .route("/",web::get().to(||{
-            	"Hello World"
-            }))
+        	.data(pool.clone())
+            .route("/",web::get().to_async(index1))
     });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
